ce103854b7c09204d5f8e0ab7f700d064a275b61
Gataitai <mpattipeilohy1@gmail.com>
2026-04-09 13:03:39 +0800

    fixed photo

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,27 @@
 import adapter from '@sveltejs/adapter-static';
+import { relative, sep } from 'node:path';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	compilerOptions: {
+		runes: ({ filename }) => {
+			const relativePath = relative(import.meta.dirname, filename);
+			const pathSegments = relativePath.toLowerCase().split(sep);
+			const isExternalLibrary = pathSegments.includes('node_modules');
+
+			return isExternalLibrary ? undefined : true;
+		}
+	},
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
 		paths: {
-			base: '/oceanguardian'
+			base: process.argv.includes('dev') ? '' : '/ogfrontend'
 		}
 	}
 };
+
+export default config;
